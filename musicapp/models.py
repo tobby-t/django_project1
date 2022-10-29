9773c0e49de3e69beddb7956ec7028478c86cd67
tobby-t <105219273+tobby-t@users.noreply.github.com>
2022-10-29 04:49:48 +0100

    first commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Lyric(models.Model):
+    cotents = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
